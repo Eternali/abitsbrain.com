3909cedd2a64b853f2115ffe757fc02a6405fe84
conrad <conrad.heidebrecht@gmail.com>
2017-10-31 17:45:28 -0400

    working on header background

--- Vue-Version/src/scripts/background.js
+++ Vue-Version/src/scripts/background.js
@@ -1,7 +1,41 @@
 let width, height
+let cols, rows, fr, scl = 10
+
+let off = {
+    x: 0,
+    y: 0,
+    z: 0
+}
+let inc = {
+    x: 0.1,
+    y: 0.1,
+    z: 0.0004
+}
+
+let particles, flowfield
+
 
 function setup () {
     width = document.documentElement.clientWidth
     height = 100
     createCanvas(width, height)
+
+    scl = 10
+    cols = floor(width / scl)
+    rows = floor(height / scl)
+    fr = createP('')
+
+    flowfield = new Array(cols * rows)
+    for (let i = 0; i < 10000; i++)
+        particles[i] = new Particle()
+
+    background(255)
+}
+
+function draw () {
+    noiseDetail(8)
+
+    off.y = 0
+
+
 }
--- Vue-Version/src/scripts/particle.js
+++ Vue-Version/src/scripts/particle.js
@@ -1,11 +1,15 @@
 class Particle {
 
-    constructor {
+    constructor () {
         pos = createVector(random(width), random(height))
         vel = createVector(0, 0)
         acc = createVector(0, 0)
         maxSpeed = 2
         prevPos = pos.copy()
+    }
+
+    update () {
+        vel.add(acc);
     }
 
 }
