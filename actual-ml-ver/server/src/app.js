97cc17200de3915f2c23e8079f2a0d301c0320bb
conrad <conrad.heidebrecht@gmail.com>
2018-01-14 00:13:44 -0500

    working on vue ml version

---
+++ actual-ml-ver/server/src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/abitsbrain', (req, res) => {
+  res.send([{
+    title: 'Abitsbrain',
+    description: 'Intro to abitsbrain!'
+  }]);
+});
+
+app.listen(process.env.PORT || 3482);
